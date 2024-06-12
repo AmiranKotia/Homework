56d88f747afa0c088a18a70abb9e2e7e9c0fa7d1
Amiran <kotiaamiran@gmail.com>
2024-06-12 18:09:05 +0400

    Add homework

--- homework.py
+++ homework.py
@@ -1,50 +1,80 @@
-# Davaleba 12
-collection = {}
+# # Davaleba 12
+# collection = {}
 
-def add_book(author, title, genre):
-    if author not in collection:
-        collection[author] = []
-    collection[author].append({'title': title, 'genre': genre})
-    print("წიგნი დაემატა კოლექციას.")
+# def add_book(author, title, genre):
+#     if author not in collection:
+#         collection[author] = []
+#     collection[author].append({'title': title, 'genre': genre})
+#     print("წიგნი დაემატა კოლექციას.")
 
-def search_author(author_name):
-    return collection.get(author_name, [])
+# def search_author(author_name):
+#     return collection.get(author_name, [])
 
-def all_books():
-    return collection
+# def all_books():
+#     return collection
 
-def main():
-    while True:
-        action = input("შეიყვანეთ რომელიმე: (add/title/author/end): ").strip().lower()
-        if action == "end":
-            break
-        elif action == "add":
-            title = input("შეიყვანეთ წიგნის სახელწოდება: ").strip()
-            author = input("შეიყვანეთ ავტორის სახელი: ").strip()
-            genre = input("შეიყვანეთ ჟანრი: ").strip()
-            add_book(author, title, genre)
-        elif action == "author":
-            author_name = ("შეიყვანეთ ავტორის სახელი: ").strip()
-            books = search_author(author_name)
-            if books:
-                print(f"წიგნები {author_name} მიერ:")
-                for book in books:
-                    print(f"სახელწოდება: {book['title']}, ჟანრი: {book['genre']}")
-            else:
-                print(f"{author_name} ავტორის სახელით წიგნი არ მოიძებნა.")
-        elif action == "title":
-            title_name = input("შეიყვანეთ წიგნის სახელწოდება: ").strip()
-            found = False
-            for author, books in collection.items():
-                for book in books:
-                    if book['title'].lower() == title_name.lower():
-                        print(f"მოიძებნა: \"{book['title']}\" {author}-ს მიერ, ჟანრი: {book['genre']}")
-                        found = True
-                if not found:
-                    print(f"წიგნი დასახელებით {title_name} არ მოიძებნა.")
-        elif action == "all":
-            print(all_books())
-        else:
-            print("არასწორი ბრძანება. გთხოვთ შეიყვანოთ რომელიმე: (add/title/author/end): ")
+# def main():
+#     while True:
+#         action = input("შეიყვანეთ რომელიმე: (add/title/author/end): ").strip().lower()
+#         if action == "end":
+#             break
+#         elif action == "add":
+#             title = input("შეიყვანეთ წიგნის სახელწოდება: ").strip()
+#             author = input("შეიყვანეთ ავტორის სახელი: ").strip()
+#             genre = input("შეიყვანეთ ჟანრი: ").strip()
+#             add_book(author, title, genre)
+#         elif action == "author":
+#             author_name = ("შეიყვანეთ ავტორის სახელი: ").strip()
+#             books = search_author(author_name)
+#             if books:
+#                 print(f"წიგნები {author_name} მიერ:")
+#                 for book in books:
+#                     print(f"სახელწოდება: {book['title']}, ჟანრი: {book['genre']}")
+#             else:
+#                 print(f"{author_name} ავტორის სახელით წიგნი არ მოიძებნა.")
+#         elif action == "title":
+#             title_name = input("შეიყვანეთ წიგნის სახელწოდება: ").strip()
+#             found = False
+#             for author, books in collection.items():
+#                 for book in books:
+#                     if book['title'].lower() == title_name.lower():
+#                         print(f"მოიძებნა: \"{book['title']}\" {author}-ს მიერ, ჟანრი: {book['genre']}")
+#                         found = True
+#                 if not found:
+#                     print(f"წიგნი დასახელებით {title_name} არ მოიძებნა.")
+#         elif action == "all":
+#             print(all_books())
+#         else:
+#             print("არასწორი ბრძანება. გთხოვთ შეიყვანოთ რომელიმე: (add/title/author/end): ")
 
-main()
+# main()
+
+
+
+# Davaleba 13:
+lst = ["red", "Green", "white", "Black", "Pink", "yellow"]
+
+file_name = "output.txt"
+
+with open(file_name, "w") as file:
+    for item in lst:
+        file.write(item + "\n")
+
+print("List contents have been saved to", file_name)
+
+
+
+def count_uppercase(file_name):
+    uppercase_count = 0
+
+    with open(file_name, "r") as file:
+        for line in file:
+            line = line.strip()
+            for item in line:
+                if item.isupper():
+                    uppercase_count += 1
+    return uppercase_count            
+    
+file_name = "output.txt"
+uppercase_count = count_uppercase(file_name)
+print("მაღალ რეგისტრში დაწერილი სიტყვების რაოდენობა: ", uppercase_count)
